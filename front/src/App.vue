a59cc1c111e37c3d7ecdc6a1eb89b02e11b9a0cd
Daniel Passy <daniel.passy@buser.com.br>
2023-04-18 10:47:37 -0300

    added front

---
+++ front/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <LoginComponent
+    v-if="step === 0"
+    @login="openUnitPage"
+  />
+  <OverviewPage
+    v-else
+    />
+</template>
+
+<script setup>
+import LoginComponent from './components/login.vue';
+import LoginComponent from './components/overview.vue';
+import {ref} from 'vue';
+
+const step = ref(0);
+const openUnitPage = () => {
+  step.value = 1;
+}
+
+
+</script>
