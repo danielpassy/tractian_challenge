faa85998cfb113931e82908f3c5280301fdaf19c
Daniel Passy <daniel.passy@buser.com.br>
2023-04-18 10:47:44 -0300

    added front

---
+++ front/src/components/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h1>Login</h1> <br>
+    <span>Email</span>
+    <input type="email" v-model="email" /> <br>
+    <span>Senha</span>
+    <input type="password" v-model="password" /> <br>
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const email = ref('');
+const password = ref('');
+
+const login = async () => {
+  res = await fetch('https://localhost:3000/api/data', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({email: email.value, password: password.value})
+})
+  $emit('login', res);
+}
+
+</script>
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
+}
+
+</style>
+
---
+++ front/src/components/overview.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    u have 10 units
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
